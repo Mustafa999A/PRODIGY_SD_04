67cc12aa1e42024aece478623345d9b55e76541b
Mustafa999A <132819859+Mustafa999A@users.noreply.github.com>
2024-07-13 21:47:15 +0530

    Update SudokuSolver.java

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class SudokuSolver {
     public static void printSudoku(int[][] sudoku) {
@@ -7,39 +8,41 @@ public class SudokuSolver {
         }
     }
 
-    public static boolean isValidMove(int[][] sudoku, int row, int col, int num) {
+    public static boolean isValidMove(int[][] sudoku, int row, int col, int num, boolean[][] rows, boolean[][] cols, boolean[][] boxes) {
         // Check if the number is not in the current row or column
-        for (int i = 0; i < 9; i++) {
-            if (sudoku[row][i] == num || sudoku[i][col] == num) {
-                return false;
-            }
+        if (rows[row][num - 1] || cols[col][num - 1]) {
+            return false;
         }
 
         // Check if the number is not in the current 3x3 grid
-        int startRow = 3 * (row / 3);
-        int startCol = 3 * (col / 3);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (sudoku[i + startRow][j + startCol] == num) {
-                    return false;
-                }
-            }
+        int boxIndex = (row / 3) * 3 + col / 3;
+        if (boxes[boxIndex][num - 1]) {
+            return false;
         }
 
         return true;
     }
 
-    public static boolean solveSudoku(int[][] sudoku) {
+    public static boolean solveSudoku(int[][] sudoku, boolean[][] rows, boolean[][] cols, boolean[][] boxes) {
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 if (sudoku[row][col] == 0) {
                     for (int num = 1; num <= 9; num++) {
-                        if (isValidMove(sudoku, row, col, num)) {
+                        if (isValidMove(sudoku, row, col, num, rows, cols, boxes)) {
                             sudoku[row][col] = num;
-                            if (solveSudoku(sudoku)) {
+                            rows[row][num - 1] = true;
+                            cols[col][num - 1] = true;
+                            int boxIndex = (row / 3) * 3 + col / 3;
+                            boxes[boxIndex][num - 1] = true;
+
+                            if (solveSudoku(sudoku, rows, cols, boxes)) {
                                 return true;
                             }
+
                             sudoku[row][col] = 0; // Backtrack
+                            rows[row][num - 1] = false;
+                            cols[col][num - 1] = false;
+                            boxes[boxIndex][num - 1] = false;
                         }
                     }
                     return false;
@@ -50,19 +53,31 @@ public class SudokuSolver {
     }
 
     public static void main(String[] args) {
-        int[][] sudokuGrid = {
-            {5, 3, 0, 0, 7, 0, 0, 0, 0},
-            {6, 0, 0, 1, 9, 5, 0, 0, 0},
-            {0, 9, 8, 0, 0, 0, 0, 6, 0},
-            {8, 0, 0, 0, 6, 0, 0, 0, 3},
-            {4, 0, 0, 8, 0, 3, 0, 0, 1},
-            {7, 0, 0, 0, 2, 0, 0, 0, 6},
-            {0, 6, 0, 0, 0, 0, 2, 8, 0},
-            {0, 0, 0, 4, 1, 9, 0, 0, 5},
-            {0, 0, 0, 0, 8, 0, 0, 7, 9}
-        };
+        int[][] sudokuGrid = new int[9][9];
+        boolean[][] rows = new boolean[9][10];
+        boolean[][] cols = new boolean[9][10];
+        boolean[][] boxes = new boolean[9][10];
 
-        if (solveSudoku(sudokuGrid)) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the elements of the 9x9 matrix:");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int num = scanner.nextInt();
+                if (num < 0 || num > 9) {
+                    System.out.println("Invalid input. Please enter a number between 0 and 9.");
+                    return;
+                }
+                sudokuGrid[i][j] = num;
+                if (num!= 0) {
+                    rows[i][num - 1] = true;
+                    cols[j][num - 1] = true;
+                    int boxIndex = (i / 3) * 3 + j / 3;
+                    boxes[boxIndex][num - 1] = true;
+                }
+            }
+        }
+
+        if (solveSudoku(sudokuGrid, rows, cols, boxes)) {
             System.out.println("Solved Sudoku:");
             printSudoku(sudokuGrid);
         } else {
@@ -70,4 +85,3 @@ public class SudokuSolver {
         }
     }
 }
-
